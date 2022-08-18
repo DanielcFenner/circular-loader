9fbb5a6e43d1c986f5fe085386ede7111bc1cdf3
Daniel Fenner <dcfenner@gmail.com>
2022-08-17 23:42:04 -0400

    looking good

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 const line = document.querySelector(".percent");
-let start = 100;
 
-setInterval(() => {
-  if (start > 0) {
-    start--;
-  } else {
-    start = 0;
-  }
-  console.log(line.style["stroke-dashoffset"]);
-  line.style["stroke-dashoffset"] = start;
-}, 500);
+setTimeout(() => {
+  line.style["stroke-dashoffset"] = 0;
+}, 1000);
